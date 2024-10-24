f854193e00e46b5f52ee7bd19bad68801eaace58
Larry Motuzis <mlauris2006@yahoo.com>
2024-10-24 17:46:26 -0500

    Fix routing for basename in GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PlayerList from './components/PlayerList';
 import StatsDisplay from './components/StatsDisplay';
 import './App.css';
@@ -8,10 +8,15 @@ function App() {
   return (
     <Router basename="/stat-tracker">
       <div className="App">
+        {/* Navigation links */}
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/stats">Stats</Link>
+        </nav>
         {/* Define the routes */}
         <Routes>
-          {/* Set PlayerList to be the default route (/) */}
-          <Route path="/" element={<PlayerList />} />
+          {/* Use exact path for home */}
+          <Route exact path="/" element={<PlayerList />} />
           {/* Route for StatsDisplay page */}
           <Route path="/stats" element={<StatsDisplay />} />
         </Routes>
@@ -21,3 +26,4 @@ function App() {
 }
 
 export default App;
+
